79ed50303c62d0e1bd5d756539f832aa5ad107ac
Aditya Shendkar <aditya.shendkar03@gmail.com>
2026-01-05 13:12:26 +0530

    add ArrayListDemo example to demonstrate ArrayList operations in Java

---
+++ Collections-Framework/List/ArrayListDemo.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        List list = new ArrayList<>();
+        list.add(100);
+        list.add(110);
+        list.add(120);
+//        System.out.println(list);
+
+        list.add(2,115);
+        System.out.println(list);
+
+        List<Integer> newList = new ArrayList<>();
+        newList.add(500);
+        newList.add(5000);
+
+        list.addAll(newList);
+        System.out.println(list);
+
+        System.out.println(list.get(4));
+
+    }
+}
