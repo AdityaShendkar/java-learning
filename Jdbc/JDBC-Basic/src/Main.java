17d55f9ba8d15a37847b25d67f218bd0175a2468
Aditya Shendkar <aditya.shendkar03@gmail.com>
2026-02-06 21:23:15 +0530

    add: enhance Main.java with employee insertion and deletion functionality

--- Jdbc/JDBC-Basic/src/Main.java
+++ Jdbc/JDBC-Basic/src/Main.java
@@ -8,47 +8,73 @@ public class Main {
         String user = " ";
         String password = " ";
 
-
-
-        try{
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Drivers Loaded Successfully");
         } catch (ClassNotFoundException e) {
             System.out.println(e.getMessage());
         }
 
-        try(Connection con = DriverManager.getConnection(url,user, password)){
+        try (Connection con = DriverManager.getConnection(url, user, password)) {
             System.out.println("Connected to Database");
-
 
             // ===================== READ =====================
             System.out.println("\n--- Existing Employees (READ) ---");
 
-            String selectQuery = "SELECT * from employees;"; //READ Operation
+            String selectQuery = "SELECT * from employees;"; // READ Operation
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery(selectQuery);
 
-            while (rs.next()){
+            while (rs.next()) {
                 int id = rs.getInt("id");
                 String name = rs.getString("name");
                 String job_title = rs.getString("job_title");
                 double salary = rs.getDouble("salary");
-                System.out.println();
                 System.out.println("============");
-                System.out.println("id:- " + id+", "+"name:- "+name+", "+ "Job-Title:- "+job_title+", "+"Salary:- "+ salary);
+                System.out.println("id:- " + id + ", " + "name:- " + name + ", " + "Job-Title:- " + job_title + ", "
+                        + "Salary:- " + salary);
             }
             rs.close();
             stmt.close();
-            con.close();
+
             System.out.println();
             System.out.println("Connection closed successfully");
 
+            // ===================== INSERT =====================
+            System.out.println("\n--- Insert New Employee Using Statement ---");
+
+            String insertQuery = "INSERT into employees(id, name, job_title, salary) VALUES(4,'D','DD',85000.0)";
+            Statement stmtInsert = con.createStatement();
+            int rowAffected = stmtInsert.executeUpdate(insertQuery);
+            // System.out.println(rowAffected);
+            if (rowAffected > 0) {
+                System.out.println("Insert successfull =" + rowAffected + " row affected");
+            } else {
+                System.out.println("Insertion failed");
+            }
+            stmtInsert.close();
+            System.out.println();
+            System.out.println("Connection closed successfully");
 
             // ===================== INSERT =====================
-            System.out.println("\n--- Insert New Employee ---");
+            System.out.println("\n--- Insert New Employee Using PreparedStatement ---");
 
+            // ===================== DELETE =====================
+            System.out.println();
+            System.out.println("\n--- Delete Employee ---");
 
-
+            String deleteQuery = "DELETE FROM employees where id = 4;";
+            Statement stmtDelete = con.createStatement();
+            int rowsAffectedByDelete = stmtDelete.executeUpdate(deleteQuery);
+            System.out.println(rowsAffectedByDelete);
+            if (rowsAffectedByDelete > 0) {
+                System.out.println("Deletion successfull =" + rowsAffectedByDelete + " row affected");
+            } else {
+                System.out.println("Deletion failed");
+            }
+            stmtDelete.close();
+            System.out.println();
+            System.out.println("Connection closed successfully");
 
         } catch (SQLException e) {
             System.out.println(e.getMessage());
