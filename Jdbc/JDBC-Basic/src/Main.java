9ba427f8f503e58e76f27cfbfb757abed97ca047
Aditya Shendkar <aditya.shendkar03@gmail.com>
2026-02-07 13:47:45 +0530

    add: implement employee update and deletion functionality with PreparedStatement in Main.java

--- Jdbc/JDBC-Basic/src/Main.java
+++ Jdbc/JDBC-Basic/src/Main.java
@@ -2,7 +2,7 @@
 import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) {
 
         String url = "jdbc:mysql://localhost:3306/JDBC_Tut";
         String user = " ";
@@ -39,6 +39,7 @@ public class Main {
 
             System.out.println();
             System.out.println("Connection closed successfully");
+            System.out.println("<><><><><><><><><>><<><<><><>");
 
             // ===================== INSERT =====================
             System.out.println("\n--- Insert New Employee Using Statement ---");
@@ -55,15 +56,42 @@ public class Main {
             stmtInsert.close();
             System.out.println();
             System.out.println("Connection closed successfully");
-
-            // ===================== INSERT =====================
+            System.out.println("<><><><><><><><><>><<><<><><>");
+            // ===================== INSERT Using PreparedStatement=====================
             System.out.println("\n--- Insert New Employee Using PreparedStatement ---");
+            String insertQueryPS = "INSERT INTO employees (id, name, job_title, salary) Values (?,?,?,?)";
+            PreparedStatement psInsert = con.prepareStatement(insertQueryPS);
+            psInsert.setInt(1, 5);
+            psInsert.setString(2, "E");
+            psInsert.setString(3, "ED");
+            psInsert.setDouble(4, 150000.0);
+            psInsert.executeUpdate();
+            System.out.println("Employee inserted successfully");
+            psInsert.close();
+            System.out.println("<><><><><><><><><>><<><<><><>");
+            // ===================== UPDATE =====================
+            System.out.println("\n--- Update Employee Salary ---");
+            String updateQuery = "UPDATE employees SET job_title = 'B-Dev', salary = 65000.0 WHERE id = 2";
+            Statement stmtUpdate = con.createStatement();
+            int rowsAffectedByUpdate = stmtUpdate.executeUpdate(updateQuery);
+            System.out.println(rowsAffectedByUpdate);
+
+            // ===================== UPDATE Using PreparedStatement=====================
+
+            System.out.println("\n--- Update Employee Salary Using PreparedStatement---");
+            String updateQueryPS = "UPDATE employees SET job_title = ?, salary = ? WHERE id = ?";
+            PreparedStatement pstmtUpdate = con.prepareStatement(updateQueryPS);
+            pstmtUpdate.setString(1, "B-Dev");
+            pstmtUpdate.setDouble(2, 655000.0);
+            pstmtUpdate.setInt(3, 2);
+            pstmtUpdate.executeUpdate();
+            System.out.println("<><><><><><><><><>><<><<><><>");
 
             // ===================== DELETE =====================
             System.out.println();
             System.out.println("\n--- Delete Employee ---");
 
-            String deleteQuery = "DELETE FROM employees where id = 4;";
+            String deleteQuery = "DELETE FROM employees where id = 5;";
             Statement stmtDelete = con.createStatement();
             int rowsAffectedByDelete = stmtDelete.executeUpdate(deleteQuery);
             System.out.println(rowsAffectedByDelete);
@@ -75,6 +103,7 @@ public class Main {
             stmtDelete.close();
             System.out.println();
             System.out.println("Connection closed successfully");
+            System.out.println("<><><><><><><><><>><<><<><><>");
 
         } catch (SQLException e) {
             System.out.println(e.getMessage());
