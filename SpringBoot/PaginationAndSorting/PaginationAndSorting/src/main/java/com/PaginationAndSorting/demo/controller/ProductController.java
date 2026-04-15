e1b0f668f46c5ae66f5257609fa0bffb6d5fc3f5
Aditya Shendkar <aditya.shendkar03@gmail.com>
2026-04-15 19:52:31 +0530

    Implement sorting and pagination methods in ProductService and update ProductController

--- SpringBoot/PaginationAndSorting/PaginationAndSorting/src/main/java/com/PaginationAndSorting/demo/controller/ProductController.java
+++ SpringBoot/PaginationAndSorting/PaginationAndSorting/src/main/java/com/PaginationAndSorting/demo/controller/ProductController.java
@@ -4,9 +4,7 @@ import com.PaginationAndSorting.demo.dto.APIResponse;
 import com.PaginationAndSorting.demo.entity.Product;
 import com.PaginationAndSorting.demo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,4 +20,16 @@ public class ProductController {
         List<Product> allProducts = service.findAllProducts();
         return new APIResponse<>(allProducts.size(), allProducts);
     }
+    @GetMapping("/{field}")
+    private APIResponse<List<Product>> getProductsWithSort(@PathVariable String field){
+        List<Product> allProducts =  service.findProductsWithSorting(field);
+        return new APIResponse<>(allProducts.size(), allProducts);
+    }
+
+    @GetMapping("/pagination/{offset}/{pageSize}")
+    private APIResponse<List<Product>> getProductsWithPagination(@PathVariable int offset,@PathVariable int pageSize){
+        List<Product> productsWithPagination = service.findProductsWithPagination(offset, pageSize);
+        return new APIResponse<>(productsWithPagination.size(),productsWithPagination);
+    }
+
 }
--- SpringBoot/PaginationAndSorting/PaginationAndSorting/src/main/java/com/PaginationAndSorting/demo/service/ProductService.java
+++ SpringBoot/PaginationAndSorting/PaginationAndSorting/src/main/java/com/PaginationAndSorting/demo/service/ProductService.java
@@ -4,6 +4,8 @@ import com.PaginationAndSorting.demo.entity.Product;
 import com.PaginationAndSorting.demo.repository.ProductRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,15 +19,23 @@ public class ProductService {
     @Autowired
     ProductRepository productrepository;
 
-    @PostConstruct
-    public void initDB() {
-        List<Product> products = IntStream.rangeClosed(1, 200)
-                .mapToObj(i -> new Product("product" + i, new Random().nextInt(100), new Random().nextInt(50000)))
-                .collect(Collectors.toList());
-        productrepository.saveAll(products);
-    }
+//    @PostConstruct
+//    public void initDB() {
+//        List<Product> products = IntStream.rangeClosed(1, 200)
+//                .mapToObj(i -> new Product("product" + i, new Random().nextInt(100), new Random().nextInt(50000)))
+//                .collect(Collectors.toList());
+//        productrepository.saveAll(products);
+//    }
 
     public List<Product> findAllProducts() {
         return productrepository.findAll();
+    }
+
+    public List<Product> findProductsWithSorting(String field){
+        return productrepository.findAll(Sort.by(Sort.Direction.ASC, field));
+    }
+
+    public List<Product> findProductsWithPagination(int offset, int pageSize){
+        return productrepository.findAll(PageRequest.of(offset,pageSize)).getContent();
     }
 }
