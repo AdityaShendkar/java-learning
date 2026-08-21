43ffa7edadca0134cd8b2244a0275ba9335a7fbe
Aditya Shendkar <aditya.shendkar03@gmail.com>
2026-08-21 13:05:14 +0530

    Refactor Home controller methods and enable security user credentials in application properties

--- Spring Security/basic-security/src/main/java/com/springsecurity/Home.java
+++ Spring Security/basic-security/src/main/java/com/springsecurity/Home.java
@@ -1,15 +1,35 @@
 package com.springsecurity;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/home")
 public class Home {
 
-    @GetMapping("/")
+    @GetMapping
     public ResponseEntity<String> getHome(){
-        return ResponseEntity.ok("Welcome To Home Page....");
+        return ResponseEntity.ok("Home Page Retrived Successfully");
     }
 
+    @PostMapping
+    public ResponseEntity<String> createHome(){
+        return ResponseEntity.ok("Home Page Created Successfully");
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateHome(){
+        return ResponseEntity.ok("Home Page Updated successfully") ;
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteHome(){
+        return ResponseEntity.ok("Home Page deleted successfully") ;
+    }
+
+    @GetMapping("/csrf")
+    public CsrfToken getToken(CsrfToken csrfToken){
+        return csrfToken;
+    }
 }
