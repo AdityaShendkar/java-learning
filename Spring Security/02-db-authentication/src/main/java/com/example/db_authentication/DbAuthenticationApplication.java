8e8d7edf2500a9f50944058c06f5f45250fdd790
Aditya Shendkar <aditya.shendkar03@gmail.com>
2026-08-25 13:44:52 +0530

    feat: Initialize Spring Security projects with basic and database authentication
    - Created a basic security project with Spring Boot, including a REST controller for home page operations (GET, POST, PUT, DELETE).
    - Added CSRF token retrieval endpoint.
    - Configured application properties for user authentication.
    - Set up Maven project structure with necessary dependencies for Spring Boot, Spring Security, and testing.
    - Implemented a database authentication project with JPA and MySQL support.
    - Included necessary dependencies for data access, security, and testing in the database authentication project.
    - Added application properties for the database authentication project.
    - Created test classes for both projects to ensure context loading.

---
+++ Spring Security/02-db-authentication/src/main/java/com/example/db_authentication/DbAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.db_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbAuthenticationApplication.class, args);
+	}
+
+}
